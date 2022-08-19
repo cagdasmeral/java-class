9ec30e7d7aaeeaca61b9ff32d5e62ac538ce8df4
Cagdas  Meral <cagdasmeral@hotmail.com>
2022-08-19 15:56:38 +0300

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/mthree/sm/helloworld/Hello.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author cagdasmeral 
+ * @since  2022-08-19 
+ *purpose: My first Hello World program in an IDE
+ */
+
+package com.mthree.sm.helloworld;
+
+
+public class Hello {
+ 
+    public static void main(String[] args) {
+        System.out.println("Hello SEMIH MERAL!");
+    }
+    
+}
